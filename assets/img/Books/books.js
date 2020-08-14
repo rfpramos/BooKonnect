e01c757aa6a35fbb4a5ac3f02b1f6facaf14675e
rosiearasa <rosearasa7522@gmail.com>
2020-08-14 00:08:06 -0400

    books object

---
+++ assets/img/Books/books.js
@@ -0,0 +1,45 @@
+
+let books=[
+  {
+    bktitle:"HIV and AIDS",
+    bkPrice:$,
+    cover:"assets\img\Books\HIV and AIDS - 179.00.jpg",
+
+  },
+  {
+    bktitle:"Integrated Science",
+    bkPrice:$,
+    cover:"assets\img\Books\Integrated Science - 7.21.jpg",
+
+  },
+  {
+    bktitle:"Peter Pan",
+    bkPrice:$,
+    cover:"assets\img\Books\Peter Pan - 5.81.jpg",
+
+  },
+  {
+    bktitle:"The Boy Who Harnessed the Wind",
+    bkPrice:$,
+    cover:"assets\img\Books\The Boy Who Harnessed the Wind - 3.59.jpg",
+
+  },
+  {
+    bktitle:"The Cure for HIV and AIDS",
+    bkPrice:$,
+    cover:"assets\img\Books\The Cure for HIV and AIDS - 24.99.jpg",
+
+  },
+  {
+    bktitle:"The Giving Tree",
+    bkPrice:$,
+    cover:"assets\img\Books\The Giving Tree - 7.96.jpg",
+
+  },
+  {
+    bktitle:"The Lorax",
+    bkPrice:$,
+    cover:"assets\img\Books\The Lorax - 3.99.jpg",
+
+  },
+];
